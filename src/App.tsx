91c00477deee0889561378bfad2cf04895f5ba2e
mohamadaltalli-mamahealth <152855449+mohamadaltalli-mamahealth@users.noreply.github.com>
2024-05-21 13:32:00 +0200

    chore: added and configured tanstackquery

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Outlet />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
